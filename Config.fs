d2bf4bdd138f5e3ab80386586ca4d43f3be3c1c3
nurazon59 <nurazon59@users.noreply.github.com>
2026-05-04 01:55:08 +0900

    feat: config file schema

---
+++ Config.fs
@@ -0,0 +1,20 @@
+open System.IO
+open FSharp.Data
+
+
+
+type ConfigJson =
+    JsonProvider<"""
+    {
+      "BaseSalary": 300000,
+      "FixedOvertime": 30,
+      "FixedOverSalary": 50000,
+      "FixedBenefits": {
+        "Housing": 30000,
+        "Transport": 10000
+      }
+    }
+    """>
+
+let Config =
+    Path.Combine(__SOURCE_DIRECTORY__, "testdata", "sample.json") |> ConfigJson.Load
